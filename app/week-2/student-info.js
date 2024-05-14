fc562c7846bae602a4cee546c781862e4f904c93
Jaeeun <jaeeun.lee@edu.sait.ca>
2024-05-13 22:54:50 -0600

    week2 update

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return(
+        <main>
+        <h1>Jaeeun Lee</h1>
+        <Link href="https://github.com/Jaeeunleesait/cprg306-assignments">My Github</Link>
+        </main>
+    );
+}
